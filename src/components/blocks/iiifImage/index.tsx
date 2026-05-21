c2e819ea21ed3cb450f820857ac39aeb15ffcba1
kerim1 <kerim.meijer@di.huc.knaw.nl>
2026-05-21 11:52:12 +0200

    Error handling in IIIF block

--- src/components/blocks/iiifImage/index.tsx
+++ src/components/blocks/iiifImage/index.tsx
@@ -27,36 +27,45 @@ function getImageHrefPromise(manifestUri: string, width: number, height?: number
 }
 
 async function getImageHref(manifestUri: string, width: number, height?: number) {
-    const manifest = await vault.loadManifest(manifestUri);
-    if (!manifest)
-        return null;
+    try {
+        if (!manifestUri.startsWith('http'))
+            manifestUri = `https://${manifestUri}`
 
-    const thumbnailRef = await getThumbnail(manifest, {vault, width, height});
-    if (!thumbnailRef?.best?.id)
-        return null;
-
-    // Type is 'unknown' when the Image API v2 is encountered
-    // Workaround to inspect the Service and build up the IIIF image URL ourselves
-    if (thumbnailRef.best.type === 'unknown') {
-        const thumbnail = await vault.get(thumbnailRef.best.id) as IIIFExternalWebResource;
-        if (!thumbnail)
+        const manifest = await vault.loadManifest(manifestUri);
+        if (!manifest)
             return null;
 
-        const service = thumbnail.service![0] as ImageService;
-        const id = service.id ?? service['@id'];
+        const thumbnailRef = await getThumbnail(manifest, {vault, width, height});
+        if (!thumbnailRef?.best?.id)
+            return null;
 
-        if (id) {
-            const size = height ? `${width},${height}` : `${width},`;
-            return `${id}/full/${size}/0/default.jpg`;
+        // Type is 'unknown' when the Image API v2 is encountered
+        // Workaround to inspect the Service and build up the IIIF image URL ourselves
+        if (thumbnailRef.best.type === 'unknown') {
+            const thumbnail = await vault.get(thumbnailRef.best.id) as IIIFExternalWebResource;
+            if (!thumbnail)
+                return null;
+
+            const service = thumbnail.service![0] as ImageService;
+            const id = service.id ?? service['@id'];
+
+            if (id) {
+                const size = height ? `${width},${height}` : `${width},`;
+                return `${id}/full/${size}/0/default.jpg`;
+            }
         }
-    }
 
-    // Found a IIIF service, so use that to obtain a IIIF image URL
-    if (thumbnailRef.best.type === 'fixed-service') {
-        return getImageFromTileSource(thumbnailRef.best, width, height).id;
-    }
+        // Found a IIIF service, so use that to obtain a IIIF image URL
+        if (thumbnailRef.best.type === 'fixed-service') {
+            return getImageFromTileSource(thumbnailRef.best, width, height).id;
+        }
 
-    return thumbnailRef.best.id;
+        return thumbnailRef.best.id;
+    }
+    catch (err: unknown) {
+        console.error(`${manifestUri}: ${err}`);
+        return null;
+    }
 }
 
 export default function IIIFImageBlockRenderer({block: {value}}: { block: IIIFImageBlock }) {
